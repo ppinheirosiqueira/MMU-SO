d4192f455c7dd0807d1911da8c0d4eeae3093fce
ppinheirosiqueira <pedropinheirodesiqueira94@gmail.com>
2023-11-23 18:54:35 -0300

    Att home
    Adicionando possibilidade de processos de tamanhos diferentes e arrumando o checkbox do aleatório com o lista de processos a serem executados

---
+++ SO/MMU/static/scripts/MaisMenosProcessos.js
@@ -0,0 +1,27 @@
+function adicionarLinha() {
+    // Cria um novo conjunto
+    var divProcessos = document.querySelector('.processos')
+    var qtdConjuntosProcessos = document.querySelectorAll('.processos').length
+    var novoProcesso = document.createElement('div')
+    novoProcesso.className = 'inputProcesso'
+    novoProcesso.innerHTML = `
+        <label for="qtdPro${qtdConjuntosProcessos + 1}">Quantidade de processos:</label>
+        <input type="number" name="qtdPro${qtdConjuntosProcessos + 1}" class="qtdPro${qtdConjuntosProcessos + 1}">
+        <label for="tamPro${qtdConjuntosProcessos + 1}">Tamanho do Processo:</label>
+        <input type="number" name="tamPro${qtdConjuntosProcessos + 1}" class="tamPro${qtdConjuntosProcessos + 1}">
+    `
+
+    // Adiciona o novo conjunto ao container
+    divProcessos.appendChild(novoProcesso)
+}
+
+function removerLinha() {
+    // Obtém todos os conjuntos de inputs
+    var conjuntosProcessos = document.querySelectorAll('.inputProcesso')
+
+    // Garante que haja pelo menos um conjunto
+    if (conjuntosProcessos.length > 1) {
+        // Remove o último conjunto
+        conjuntosProcessos[conjuntosProcessos.length - 1].remove()
+    }
+}
